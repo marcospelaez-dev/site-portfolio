8b857229f647d39e95a838e7b9a0830534ec0079
Marcos Sousa <pelaez.marcos@gmail.com>
2023-11-15 20:00:01 -0300

    configurando-header-tablet

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,20 @@
 
 function menuAppear(){
+    
     let navMenu = document.getElementById("menuMobile");
+    let malha = document.getElementById("malha");
 
-    if(navMenu.classList.contains('menu-closed')){
+    if(navMenu.classList.contains('menu-closed') && malha.classList.contains('menu-closed')){
         navMenu.classList.remove('menu-closed');
         navMenu.classList.add('menu-opened');
+        malha.classList.remove('menu-closed');
+        malha.classList.add('menu-opened');
+        
     }else{
         navMenu.classList.remove('menu-opened');
         navMenu.classList.add('menu-closed');
+        malha.classList.remove('menu-opened');
+        malha.classList.add('menu-closed');
     }
+    
 }
