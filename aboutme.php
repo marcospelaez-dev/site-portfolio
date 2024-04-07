f6efe75c80a267ffe2e4f1d4be561a548ca09dc1
Marcos Sousa <pelaez.marcos@gmail.com>
2024-04-07 16:00:27 -0300

    aboutme-page
    config header part

--- aboutme.php
+++ aboutme.php
@@ -4,7 +4,14 @@
 
 <div class="div-line"></div>
         
-       
+    <main>
+        <header>
+            <img class="perfil-sobre" src="./assets/img/perfil-sobre.png" alt="Foto perfil Marcos Pelaez">
+            <h1 class="title-perfil">Marcos Antonio Pelaez de Sousa</h1>
+            <p class="job-perfil">Web Designer</p>
+            <p class="job-perfil">Design Gráfico</p>
+        </header>
+    </main>
 
 <?php require_once('./contato.php') ?>
 
