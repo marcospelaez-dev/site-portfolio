f9080805a1bc1fe76a9ff4721ae8a7f3c7019dba
Marcos Sousa <pelaez.marcos@gmail.com>
2024-04-21 18:36:01 -0300

    portfolio page
    Complete Portfolio Page with php and css plus emages.

--- portfolio.php
+++ portfolio.php
@@ -1,6 +1,5 @@
 
 
-
 <?php require_once('./header.php') ?>
 
 <div class="div-line"></div>
@@ -16,8 +15,42 @@
         <hr class="divider"/>
 
         <section class="vision-portfolio">
-
             <h2>Portfolio</h2>
+
+            <div class="container-view">
+                <section class="port-view">
+
+                    <div class="view-picture"> <!-- first link portfolio HBO DIO -->
+                        <a href="#">VER</a>
+                        <div class="back-view"></div>
+                        <div class="photo-view">
+                            <img src="./assets/img/foto-hbo.png" alt="site HBO - DIO">
+                        </div>
+                    </div>
+                    <div class="view-text">
+                        <h3>Reprodução Site HBO MAX</h3>
+                        <p>Desafio Formação CSS Developer - DIO</p>
+                    </div> <!-- end first link portfolio HBO DIO-->
+                </section>
+
+                <section class="port-view">
+
+                    <div class="view-picture"> <!-- second link portfolio - Viajem -->
+                        <a href="#">VER</a>
+                        <div class="back-view"></div>
+                        <div class="photo-view">
+                            <img src="./assets/img/foto-viagem.png" alt="site template Viajem">
+                        </div>
+                    </div>
+                    <div class="view-text">
+                        <h3>Design fictício Site Viajem</h3>
+                        <p>Modelo de site para mostra do meu trabalho</p>
+                    </div> <!-- End second link portfolio - Viajem -->
+
+                </section>
+            </div>
+
+            
 
         </section>
     </main>
