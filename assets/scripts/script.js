0e0acbcbdfa52b0a70ecb072518ac0a7e347dc67
Marcos Sousa <pelaez.marcos@gmail.com>
2024-05-13 17:02:42 -0300

    javascript refatorado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,22 +1,14 @@
 
 //Behavior menu open and close
-function menuAppear(){
-    
-    let navMenu = document.getElementById("menuMobile");
-    let malha = document.getElementById("malha");
+function menuAppear() {
+    const navMenu = document.getElementById("menuMobile");
+    const malha = document.getElementById("malha");
+    const isClosed = navMenu.classList.contains('menu-closed');
 
-    if(navMenu.classList.contains('menu-closed') && malha.classList.contains('menu-closed')){
-        navMenu.classList.remove('menu-closed');
-        navMenu.classList.add('menu-opened');
-        malha.classList.remove('menu-closed');
-        malha.classList.add('menu-opened');
-        
-    }else{
-        navMenu.classList.remove('menu-opened');
-        navMenu.classList.add('menu-closed');
-        malha.classList.remove('menu-opened');
-        malha.classList.add('menu-closed');
-    }
-    
+    navMenu.classList.toggle('menu-closed', !isClosed);
+    navMenu.classList.toggle('menu-opened', isClosed);
+    malha.classList.toggle('menu-closed', !isClosed);
+    malha.classList.toggle('menu-opened', isClosed);
 }
+
 
